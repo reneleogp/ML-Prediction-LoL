a135fe08b69d80698d76c8b977f4c3634c2f01ae
Rene <reneleogp0305@gmail.com>
2022-01-26 17:32:37 -0500

    Model saved

--- src/app.py
+++ src/app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import joblib
+
 
 df = pd.DataFrame(
     np.random.randn(50, 20),
     columns=('col %d' % i for i in range(20)))
 
 st.dataframe(df)  # Same as st.write(df)
+
+model = joblib.load('src/finalized_model.sav')
